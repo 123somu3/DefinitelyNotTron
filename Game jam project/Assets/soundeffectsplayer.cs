67bfc6239d01b50da000573cea536e2c50de791b
Naswithaa <naswitha855@gmail.com>
2024-08-12 19:56:53 +0530

    aswitha

---
+++ Game jam project/Assets/soundeffectsplayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class soundeffectsplayer : MonoBehaviour
+{
+    public AudioSource src;
+    public AudioClip sfx1, sfx2;
+
+
+    public void Button1()
+    {
+        src.clip= sfx1;
+        src.Play();
+    }
+
+    public void Button2()
+    {
+        src.clip = sfx2;
+        src.Play();
+    }
+
+}
